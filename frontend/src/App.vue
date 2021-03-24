c4bda40baeaea7eb63f2c37abc54e91c70b6408b
Tiffany-Kwan <haoyang.guan@ucdconnect.ie>
2021-03-24 16:23:51 +0800

    improve the main page

---
+++ frontend/src/App.vue
@@ -0,0 +1,124 @@
+<template>
+
+  
+  <el-container>
+    <el-header>
+    
+  
+
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b">
+  <el-menu-item index="1"><a href="/chat">Smart Chatting</a></el-menu-item>
+  <el-menu-item index="2"><a href="/recommend">Recommendation for You</a></el-menu-item>
+  <el-menu-item index="3"><a href="/house">Housing Resources</a></el-menu-item>
+    <!-- <el-menu-item index="2-1">选项1</el-menu-item>
+    <el-menu-item index="2-2">选项2</el-menu-item>
+    <el-menu-item index="2-3">选项3</el-menu-item>
+    <el-submenu index="2-4">
+      <template slot="title">选项4</template>
+      <el-menu-item index="2-4-1">选项1</el-menu-item>
+      <el-menu-item index="2-4-2">选项2</el-menu-item>
+      <el-menu-item index="2-4-3">选项3</el-menu-item> -->
+    <!-- </el-submenu> -->
+  
+  <!-- <el-menu-item index="3" disabled>消息中心</el-menu-item> -->
+  
+</el-menu>
+<div class="line"></div>
+
+
+</el-header>
+
+
+    <el-container>
+      <!-- <el-aside width="500px">
+      <el-row class="tac">
+        
+        <el-col :span="12">
+          <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+            
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="1-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="1-4-1">选项1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-menu-item index="2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="3" disabled>
+              <i class="el-icon-document"></i>
+              <span slot="title">导航三</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <i class="el-icon-setting"></i>
+              <span slot="title">导航四</span>
+            </el-menu-item>
+          </el-menu>
+        </el-col>
+      </el-row>
+      </el-aside> -->
+
+
+      <el-main>
+        <div id="app">
+          <div id="nav">
+            <router-link to="/">Home</router-link> |
+            <!-- <router-link to="/chat">Chat</router-link> -->
+          </div>
+          <router-view/>
+        </div>
+      </el-main>
+    </el-container>
+  </el-container>
+  
+  
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 60px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+
+
+
---
+++ frontend/src/router/index.js
@@ -0,0 +1,54 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import HouseList from "../components/HouseList"
+import Recommend from "../components/Recommend"
+import Chat from "../components/Chat";
+
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/Chat',
+    name: 'Chat',
+    component: Chat
+  },
+  {
+    path: '/Chat',
+    name: 'Chat',
+    component: Chat
+  },
+  {
+    path: '/house',
+    name: 'house',
+    component: HouseList
+  },
+  {
+    path: '/recommend',
+    name: 'recommend',
+    component: Recommend
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
