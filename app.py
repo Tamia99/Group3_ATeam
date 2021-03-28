98b01ecada58c9b389d5130370548010beed0801
17206184 <17610409270@163.com>
2021-03-28 20:48:48 +0800

    connect to database

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template, jsonify
+
+from flask import Flask, render_template, jsonify, request, json, Response
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 from random import *
+import config
 
 app = Flask(__name__,
             static_folder="frontend/dist/static",
             template_folder="frontend/dist")
+
+app.config.from_object(config)
+db = SQLAlchemy(app)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route('/', defaults = {'path': ''})
@@ -18,6 +24,21 @@ def random_number():
         'randomNumber': randint(1,100)
     }
     return jsonify(response)
+
+
+class Article(db.Model):
+    __tablename__ = 'article'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(10), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+db.create_all()
+
+# title = request.args.get("title")
+# content = request.args.get("content")
+#测试数据库连接并新增一条数据
+article = Article(title = "333", content = "jfnsid")
+db.session.add(article)
+db.session.commit()
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,10 @@
+DIALECT = 'mysql'
+DRIVER = 'pymysql'
+USERNAME = 'root'
+PASSWORD = 'Nancy990927'
+HOST = 'bj-cynosdbmysql-grp-d2ehu1js.sql.tencentcdb.com'
+PORT = '24040'
+DATABASE = 'realEstate'
+SQLALCHEMY_TRACK_MODIFICATIONS=False
+SQLALCHEMY_DATABASE_URI = "{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT,
+                                                                       DATABASE)
